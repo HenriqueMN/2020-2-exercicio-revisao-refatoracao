7259ef91e608aa979bf497d18b0221c8760a06aa
HenriqueMN <79935950+HenriqueMN@users.noreply.github.com>
2021-03-02 21:08:41 -0300

    Update EncomendaNormal.hpp

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -3,28 +3,14 @@
 
 #include "Encomenda.hpp"
 #include "Cliente.hpp"
-using namespace std;
 
 class EncomendaNormal: public Encomenda{
 
 	private:
-		
-	double calcula(){
+        
+		double calcula();
 
-		double x = peso * custoKg;
-
-		return x;
-	}
-
-	void print(){
-
-		Encomenda::print();
-		std::cout << "[Encomenda Normal]" << endl;
-		std::cout << "  Peso: " << peso << endl
-			<< "  Custo por kg: " << custoKg << endl
-			<< "  Custo total: " << T << endl;
-
-	}
+        	void print() override; // imprime os dados da encomenda, bem como seu tipo
 
 };
 
